2db09101fdb4351e516c9b546d4267c26df1650c
teohbenji <teohbenjamin1@gmail.com>
2024-05-29 01:39:32 +0800

    Added token image change functionality

--- src/problem2/script.js
+++ src/problem2/script.js
@@ -1,3 +1,29 @@
+window.onload = function() {
+    //Hide both images initially
+    let elementsArr = Array.from(document.getElementsByClassName("token-image"));
+    
+    elementsArr.map(element => {
+        element.style.display = 'none';
+    });
+
+    //Display crypto token when dropdown item selected
+    var e = document.getElementById("dropdownToken1");
+    e.onchange = function() {
+        console.log('images/' + e.options[e.selectedIndex].text + '.svg');
+        let tokenImage1 = document.getElementById("imageToken1");
+        tokenImage1.style.display = 'block';
+        tokenImage1.src = 'images/' + e.options[e.selectedIndex].text + '.svg';
+    };
+
+    var f = document.getElementById("dropdownToken2");
+    f.onchange = function() {
+        console.log('images/' + e.options[e.selectedIndex].text + '.svg');
+        let tokenImage2 = document.getElementById("imageToken2");
+        tokenImage2.style.display = 'block';
+        tokenImage2.src = 'images/' + e.options[e.selectedIndex].text + '.svg';
+    };
+};
+
 function fetchUSDValueOfCoin(coinID) {
     const apiUrl = 'https://api.coingecko.com/api/v3/coins/' + coinID;
 
